a98c09e48d813fa2c601ed52a77bf45e3767f4d6
vanessaxuuan <64406694+vanessaxuuan@users.noreply.github.com>
2025-05-16 21:45:55 +0800

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   devIndicators: false,
-  output: 'export',
-  basePath: '/myportfolio',
   images: {
     unoptimized: true,
   },
